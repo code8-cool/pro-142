732ae9631a673ad08052d5cd14bafe76ee339edc
code8-cool <136864069+code8-cool@users.noreply.github.com>
2024-06-15 10:14:48 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song1 = ""
 song2 = ""
 
+scoreRightWrist = 0
+scoreLeftWrist = 0
+
 RightWristX = 0
 RightWristY = 0
 
@@ -35,6 +38,8 @@ if(results.length > 0)
 {
 console.log(results);
 
+scoreLeftWrist = results[0].pose.keypoints[9].score;
+
 RightWristX = results[0].pose.rightWrist.x;
 RightWristY = results[0].pose.rightWrist.y;
 console.log("rightWristX = " + RightWristX + " rightWristY = "+ RightWristY)
@@ -50,6 +55,27 @@ console.log("leftWristX = " + LeftWristX + " leftWristY = "+ LeftWristY)
 
 function draw() {
     image(video, 0, 0, 600, 500);
+    fill("#FFD700");
+    stroke("#FFD700");
+
+    
+    if (scoreLeftWrist > 0.2) {
+        circle(LeftWristX, LeftWristY, 20);
+
+        num_leftWrist = Number(LeftWristY);
+        song = song1;
+        //console.log(vol);
+        song.setVolume(vol);
+      
+        
+    if(song1.isPlaying() = false){
+    song1.play();
+    song2.stop();
+    }
+
+    document.getElementById("song").innerHTML = "Song : " + song;
+
+}
 }
 
 function play() 
